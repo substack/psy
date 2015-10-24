71603ddcbc283bb0729a4b36572191b5168fe904
James Halliday <mail@substack.net>
2015-10-23 17:22:12 -0700

    using auto-daemon

--- cmd.js
+++ cmd.js
@@ -2,35 +2,23 @@
 
 var fs = require('fs')
 var path = require('path')
-var net = require('net')
 
 var minimist = require('minimist')
 var argv = minimist(process.argv.slice(2), {
   alias: { h: 'help', s: 'sockfile', l: 'logfile', f: 'follow' },
-  boolean: [ 'f' ]
+  boolean: [ 'f', 'debug' ]
 })
 var cmd = argv._[0]
 if (cmd === 'help' || argv.help) return usage(0)
 
 var defined = require('defined')
-var has = require('has')
-var xtend = require('xtend')
-var once = require('once')
 var timeago = require('timeago')
 var table = require('text-table')
-var sprintf = require('sprintf')
-var through = require('through2')
 var configDir = require('xdg-basedir').config
-var sliceFile = require('slice-file')
-
-var rpc = require('rpc-stream')
-var respawn = require('respawn-group')
-
-var spawn = require('child_process').spawn
 var randomBytes = require('crypto').randomBytes
 
 var METHODS = [
-  'start', 'stop', 'restart', 'remove', 'list', 'log',
+  'start', 'stop', 'restart', 'remove', 'list', 'log:s',
   'close', 'kill', 'reset'
 ]
 
@@ -52,475 +40,101 @@ var statefile = defined(
 mkdirp.sync(path.dirname(sockfile))
 
 if (cmd === 'version' || (!cmd && argv.version)) {
-  console.log(require('./package.json').version)
-} else if (cmd === 'start') {
-  var name = defined(argv.name, argv.n, randomBytes(4).toString('hex'))
-  var opts = {
-    cwd: defined(argv.cwd, process.cwd()),
-    env: argv.env,
-    maxRestarts: defined(argv.maxRestarts, -1),
-    sleep: defined(argv.sleep, 0),
-    logfile: argv.logfile
-  }
-  getGroup(function (err, group) {
-    if (err) return error(err)
-    group.list(function (err, gs) {
-      if (err) return error(err)
-      for (var i = 0; i < gs.length; i++) {
-        if (gs[i].id !== name) continue
-        if (gs[i].status === 'running') {
-          return error('A process called ' + JSON.stringify(name)
-            + ' is already running.')
-        } else {
-          group.remove(name, function (err) {
-            if (err) console.error(err)
-            cstart(group)
-          })
-        }
-        return
-      }
-      cstart(group)
-    })
-  })
-  function cstart (group) {
-    group.start(name, argv._.slice(1), opts, function (err) {
-      if (err) console.error(err)
+  return console.log(require('./package.json').version)
+}
+
+var autod = require('auto-daemon')
+var opts = {
+  rpcfile: path.join(__dirname, 'server.js'),
+  sockfile: sockfile,
+  methods: METHODS,
+  debug: argv.debug,
+  args: [
+    '--sockfile', sockfile,
+    '--pidfile', pidfile,
+    '--statefile', statefile
+  ]
+}
+autod(opts, function (err, r, c) {
+  if (cmd === 'start') {
+    var name = defined(argv.name, argv.n, randomBytes(4).toString('hex'))
+    var opts = {
+      cwd: defined(argv.cwd, process.cwd()),
+      env: argv.env,
+      maxRestarts: defined(argv.maxRestarts, -1),
+      sleep: defined(argv.sleep, 0),
+      logfile: argv.logfile
+    }
+    r.start(name, argv._.slice(1), opts, function (err) {
+      if (err && err.info) error(err.info)
+      else if (err) error(err)
       else if (!argv.name && !argv.n) console.log(name)
-      group.disconnect()
+      c.end()
+      process.exit()
     })
-  }
-} else if (cmd === 'stop') {
-  var name = defined(argv.name, argv.n, argv._[1])
-  getGroup(function (err, group) {
-    if (err) return error(err)
-    group.stop(name, function (err) {
+  } else if (cmd === 'stop') {
+    var name = defined(argv.name, argv.n, argv._[1])
+    r.stop(name, function (err) {
+      if (err) error(err)
+      c.end()
+    })
+  } else if (cmd === 'restart') {
+    var name = defined(argv.name, argv.n, argv._[1])
+    r.restart(name, function (err) {
       if (err) return error(err)
-      group.disconnect()
+      c.end()
     })
-  })
-} else if (cmd === 'restart') {
-  var name = defined(argv.name, argv.n, argv._[1])
-  getGroup(function (err, group) {
-    if (err) return error(err)
-    group.restart(name, function (err) {
-      if (err) console.error(err)
-      group.disconnect()
+  } else if (cmd === 'rm' || cmd === 'remove') {
+    var name = defined(argv.name, argv.n, argv._[1])
+    r.remove(name, function (err) {
+      if (err) return error(err)
+      c.end()
     })
-  })
-} else if (cmd === 'rm' || cmd === 'remove') {
-  var name = defined(argv.name, argv.n, argv._[1])
-  getGroup(function (err, group) {
-    if (err) return error(err)
-    group.remove(name, function (err) {
-      if (err) console.error(err)
-      group.disconnect()
-    })
-  })
-} else if (cmd === 'list' || cmd === 'ls') {
-  getGroup(function (err, group) {
-    if (err) return error(err)
-    group.list(function (err, items) {
+  } else if (cmd === 'list' || cmd === 'ls') {
+    r.list(function (err, items) {
       if (err) console.error(err)
       else process.stdout.write(formatList(items))
-      group.disconnect()
+      c.end()
     })
-  })
-} else if (cmd === 'log') {
-  var name = defined(argv.name, argv._[1])
-  getGroup(function (err, group) {
-    if (err) return error(err)
-    group.log(name, {
+  } else if (cmd === 'log') {
+    var name = defined(argv.name, argv._[1])
+    var stream = r.log(name, {
       n: argv.n,
       N: argv.N,
       follow: argv.follow
-    }, function (err) {
-      if (err) console.error(err)
-      //group.disconnect()
     })
-  })
-} else if (cmd === 'server') {
-  fs.stat(sockfile, function (err) {
-    if (!err) fs.unlink(sockfile, fstart)
-    else fstart()
-  })
-  function fstart (err) {
-    start({ autoclose: defined(argv.autoclose, false) }, function (err) {
+    stream.on('end', function () {
+      c.end()
+    })
+    stream.pipe(process.stdout)
+  } else if (cmd === 'server') {
+    // todo
+  } else if (cmd === 'daemon') {
+    // todo
+  } else if (cmd === 'pid') {
+    fs.readFile(pidfile, 'utf8', function (err, pid) {
       if (err) error(err)
+      else console.log(pid)
     })
-  }
-} else if (cmd === 'daemon') {
-  daemon({ autoclose: false })
-} else if (cmd === 'pid') {
-  fs.readFile(pidfile, 'utf8', function (err, pid) {
-    if (err) error(err)
-    else console.log(pid)
-  })
-} else if (cmd === 'close') {
-  getGroup(function (err, group) {
-    if (err) return error(err)
-    group.close(function () {
-      group.disconnect()
+  } else if (cmd === 'close') {
+    r.close(function () {
+      c.end()
     })
-  })
-} else if (cmd === 'reset') { 
-  fs.unlink(statefile, function () {
-    getGroup(function (err, group) {
-      if (err) return error(err)
-      group.reset(function () {
-        group.disconnect()
+  } else if (cmd === 'kill') {
+    r.kill(function () { c.end() })
+  } else if (cmd === 'reset') { 
+    fs.unlink(statefile, function () {
+      r.reset(function () {
+        c.end()
       })
     })
-  })
-} else usage(1)
+  } else usage(1)
+})
 
 function usage (code) {
   var r = fs.createReadStream(path.join(__dirname, 'usage.txt'))
   if (code) r.once('end', function () { process.exit(code) })
   r.pipe(process.stdout)
-}
-
-function start (opts, cb) {
-  if (typeof opts === 'function') {
-    cb = opts
-    opts = {}
-  }
-  cb = once(cb)
-  var group = respawn()
-  var extra = {}
-  var logging = {}
-  var logfiles = {}
-  var linestate = {}
-  group.on('stdout', ondata)
-  group.on('stderr', ondata)
-
-  group.on('start', onev('start'))
-  group.on('stop', onev('stop'))
-  group.on('restart', onev('restart'))
-  group.on('crash', onev('crash'))
-  group.on('sleep', onev('sleep'))
-  group.on('spawn', onev('spawn', 'PID %d', 'pid'))
-  group.on('exit', onev('exit', '%s'))
-  group.on('warn', onev('warn', '%s'))
-
-  function ondata (mon, buf) {
-    if (!has(logging, mon.id)) return
-    var outputs = logging[mon.id]
-    outputs.forEach(function (out) { out.write(buf) })
-    linestate[mon.id] = buf[buf.length-1] === 10 // \n
-  }
-
-  function checkpoint(cb) {
-    var src = JSON.stringify(group.list().map(function (e) {
-      return {
-        id: e.id,
-        status: e.status,
-        command: e.command,
-        cwd: e.cwd,
-        env: e.env,
-        maxRestarts: e.maxRestarts,
-        sleep: e.sleep,
-        extra: extra[e.id]
-      }
-    }))
-    fs.writeFile(statefile, src, function (err) {
-      if (cb && typeof cb === 'function') cb(err)
-    })
-  }
-
-  function onev (name, fmt) {
-    name = name.toUpperCase()
-    var props = [].slice.call(arguments, 2)
-
-    return function (mon) {
-      if (!has(logging, mon.id)) return
-      var outputs = logging[mon.id]
-      var args = [].slice.call(arguments, 1)
-      for (var i = 0; i < props.length; i++) {
-        args[i] = args[i][props[i]]
-      }
-
-      outputs.forEach(function (out) {
-        var pre = (linestate[mon.id] ? '' : '\n') + '!!! PROCESS '
-        if (fmt) {
-          var str = sprintf.apply(null, [fmt].concat(args).filter(Boolean))
-          out.write(pre + name + ': ' + str + '\n')
-        } else {
-          out.write(pre + name + '\n')
-        }
-        linestate[mon.id] = true
-      })
-    }
-  }
-
-  var iface = {
-    list: function (cb) {
-      var items = group.list().map(function (item) {
-        var ref = xtend(item, {})
-        delete ref.domain
-        delete ref.child
-        delete ref._events
-        delete ref._eventsCount
-        delete ref._maxListeners
-        delete ref.timeout
-
-        if (has(extra, item.id)) return xtend(ref, extra[item.id])
-        else return ref
-      })
-      if (typeof cb === 'function') cb(null, items)
-    },
-    start: function (name, command, opts, cb) {
-      if (typeof opts === 'function') {
-        cb = opts
-        opts = {}
-      }
-      startgroup(name, command, opts, function(err) {
-        if (err) return cb(err)
-        checkpoint(cb)
-      })
-    },
-    stop: function (name, cb) {
-      group.stop(name, function (err) {
-        if (err) return cb(err)
-        checkpoint(cb)
-      })
-    },
-    restart: function (name, cb) {
-      group.restart(name)
-      checkpoint(cb)
-    },
-    remove: function (name, cb) {
-      group.remove(name)
-      checkpoint(cb)
-    },
-    kill: function () {
-      server.close()
-      process.exit()
-    },
-    close: function (cb) {
-      group.list().forEach(function (item) {
-        group.remove(item.id)
-      })
-      server.close()
-      if (cb && typeof cb === 'function') cb()
-    },
-    reset: function (cb) {
-      server.close()
-      fs.unlink(pidfile)
-      fs.unlink(sockfile)
-      fs.unlink(statefile)
-    }
-  }
-
-  function startgroup(name, command, opts, cb) {
-    if (opts.logfile && !has(logging, name)) {
-      logfiles[name] = opts.logfile
-      var w = fs.createWriteStream(opts.logfile, { flags: 'a' })
-      w.once('error', function (err) {
-        console.error(err.stack || err)
-      })
-      logging[name] = [ w ]
-      linestate[name] = true
-      extra[name] = { logfile: opts.logfile }
-    }
-    if (!group.get(name)) group.add(name, command, opts)
-    group.start(name, opts)
-    if (cb && typeof cb === 'function') cb()
-  }
-
-  readState(function (err, state) {
-    if (err) return cb(err)
-    state.forEach(function (e) {
-      startgroup(e.id, e.command, {
-        cwd: e.cwd,
-        env: e.env,
-        logfile: e.logfile, 
-        maxRestarts: e.maxRestarts,
-        sleep: e.sleep
-      })
-    })
-  })
-
-  var connected = 0
-  var server = net.createServer(function (stream) {
-    connected ++
-    var isconnected = true
-    stream.on('error', function () {})
-
-    iface.log = function (name, opts, cb) {
-      if (typeof opts === 'function') {
-        cb = opts
-        opts = {}
-      }
-      if (!opts) opts = {}
-      if (!has(logging, name)) {
-        logging[name] = []
-        linestate[name] = true
-      }
-      var log = through(writec)
-      if (defined(opts.n, opts.N) !== undefined && logfiles[name]) {
-        showlines()
-      }
-      if (defined(opts.n, opts.N) === undefined || opts.follow) {
-        logging[name].push(log)
-        stream.once('_cleanup', function () {
-          var ix = logging[name].indexOf(log)
-          if (ix >= 0) logging[name].splice(ix, 1)
-        })
-      }
-
-      function writec (buf, enc, next) {
-        client.write(buf.toString('base64'))
-        next()
-      }
-
-      function showlines () {
-        log.pause()
-        var sf = sliceFile(logfiles[name])
-        var args = []
-        if (/,/.test(opts.n)) {
-          args = opts.n.split(',').map(function (s) {
-            return -Number(s)
-          })
-        } else if (opts.n !== undefined) {
-          args[0] = -opts.n
-        } else if (/,/.test(opts.N)) {
-          args = opts.N.split(',').map(Number)
-        } else {
-          args[0] = opts.N
-        }
-        sf.slice.apply(sf, args).pipe(through(writec, function () {
-          log.resume()
-          if (logging[name] && logging[name].indexOf(log) < 0) {
-            stream.end()
-          }
-        }))
-      }
-    }
-    var rstream = rpc(iface)
-    var client = rstream.wrap(['write'])
-    stream.pipe(rstream).pipe(stream)
-
-    stream.once('end', onend)
-    stream.once('error', onend)
-    stream.once('close', onend)
-    function onend () {
-      if (!isconnected) return
-      isconnected = false
-      connected -= 1
-      stream.emit('_cleanup')
-
-      if (opts.autoclose === false) return
-      if (connected === 0 && group.list().length === 0) {
-        setTimeout(function () {
-          if (connected !== 0) return
-          if (group.list().length > 0) return
-          server.close()
-        }, 1000)
-      }
-    }
-  })
-  server.listen(sockfile, function () {
-    cb(null, group)
-    if (argv.parentpid) process.kill(argv.parentpid, 'SIGUSR2')
-  })
-  server.once('error', cb)
-
-  process.once('exit', function () {
-    fs.unlinkSync(pidfile)
-    fs.unlinkSync(sockfile)
-  })
-}
-
-function daemon (opts, cb) {
-  if (typeof opts === 'function') {
-    cb = opts
-    opts = {}
-  }
-  if (!opts) opts = {}
-  cb = once(cb || function () {})
-  var args = [
-    __filename, 'server',
-    '--pidfile', pidfile,
-    '--sockfile', sockfile,
-    '--parentpid', process.pid,
-    '--statefile', statefile,
-    '--autoclose', defined(argv.autoclose, 'true')
-  ]
-  if (opts.autoclose !== undefined) {
-    args.push('--autoclose', String(opts.autoclose))
-  }
-  var ps = spawn(process.execPath, args, {
-    stdio: 'ignore',
-    detached: true
-  })
-  var pending = 2
-  fs.writeFile(pidfile, String(ps.pid), function (err) {
-    if (err) cb(err)
-    else if (--pending === 0) cb()
-  })
-  ps.once('exit', function (code) {
-    cb(new Error('exited with code: ' + code))
-  })
-  process.once('SIGUSR2', function () {
-    ps.unref()
-    if (--pending === 0) cb()
-  })
-}
-
-function connect (cb_) {
-  var cb = once(function (err, r) {
-    process.nextTick(function () {
-      process.removeListener('uncaughtException', onuncaught)
-    })
-    cb_(err, r)
-  })
-  process.once('uncaughtException', onuncaught)
-  function onuncaught (err) {
-    // needed because some core bug with catching errors in unix sockets
-    if (err && err.code === 'ECONNREFUSED') {}
-    else {
-      console.error(err.stack || err)
-      process.exit(1)
-    }
-  }
-  var c = net.connect(sockfile)
-  var client = rpc({
-    write: function (buf) {
-      process.stdout.write(Buffer(buf, 'base64'))
-    }
-  })
- 
-  var r = client.wrap(METHODS)
-  r.disconnect = function () { c.destroy() }
-  c.once('connect', function () {
-    cb(null, r)
-  })
-  c.once('error', cb)
-  c.pipe(client).pipe(c)
-}
-
-function getGroup (cb) {
-  fs.stat(sockfile, function (err) {
-    if (err) return connectAndDaemonize(cb)
-    connect(function (err, r) {
-      if (err) return connectAndDaemonize(cb)
-      else cb(null, r)
-    })
-  })
-}
-
-function connectAndDaemonize (cb) {
-  fs.unlink(sockfile, function () {
-    daemon(function (err) {
-      if (err) cb(err)
-      else connect(cb)
-    })
-  })
-}
-
-function error (err) {
-  console.error(err.stack || err)
-  process.exit(1)
 }
 
 function formatList (items) {
@@ -538,15 +152,7 @@ function formatList (items) {
   })) + (items.length ? '\n' : '')
 }
 
-function readState (cb) {
-  fs.readFile(statefile, 'utf8', function (err, src) {
-    if (err && err.code === 'ENOENT') src = '[]'
-    else if (err) return cb(err)
-
-    try { var state = JSON.parse(src) }
-    catch (err) { return cb(err) }
-
-    if (!Array.isArray(state)) cb(null, [])
-    else cb(null, state)
-  })
+function error (err) {
+  console.error(err.stack || err)
+  process.exit(1)
 }
---
+++ server.js
@@ -0,0 +1,303 @@
+var respawn = require('respawn-group')
+var xtend = require('xtend')
+var mkdirp = require('mkdirp')
+var minimist = require('minimist')
+var onend = require('end-of-stream')
+var sliceFile = require('slice-file')
+var once = require('once')
+var fs = require('fs')
+var defined = require('defined')
+var through = require('through2')
+var has = require('has')
+var sprintf = require('sprintf')
+var inherits = require('inherits')
+var EventEmitter = require('events').EventEmitter
+
+var connected = 0
+var psy = null
+
+module.exports = function (server, stream, args) {
+  if (!psy) psy = new Psy(minimist(args))
+  psy.on('error', function (err) { stream.emit('error', err) })
+  connected++
+  onend(stream, function () {
+    if (--connected === 0 && psy.group.list().length === 0) {
+      setTimeout(function () {
+        if (connected !== 0) return
+        if (psy.group.list().length > 0) return
+        server.close()
+      }, 1000)
+    }
+  })
+  return {
+    start: psy.start.bind(psy),
+    stop: psy.stop.bind(psy),
+    restart: psy.restart.bind(psy),
+    remove: psy.remove.bind(psy),
+    log: psy.log.bind(psy),
+    list: psy.list.bind(psy),
+    close: function (cb) {
+      if (typeof cb !== 'function') cb = noop
+      psy.group.list().forEach(function (item) {
+        psy.group.remove(item.id)
+      })
+      server.close()
+      cb()
+    },
+    reset: function (cb) {
+      if (typeof cb !== 'function') cb = noop
+      server.close()
+      var pending = 3
+      fs.unlink(psy.pidfile, done)
+      fs.unlink(psy.sockfile, done)
+      fs.unlink(psy.statefile, done)
+      function done (err) {
+        if (err) cb(err)
+        else if (--pending === 0) cb()
+      }
+    },
+    kill: function (cb) {
+      server.close()
+      cb()
+      process.exit()
+    }
+  }
+}
+
+inherits(Psy, EventEmitter)
+
+function Psy (opts) {
+  if (!(this instanceof Psy)) return new Psy(opts)
+  var self = this
+
+  self.sockfile = opts.sockfile
+  self.pidfile = opts.pidfile
+  self.statefile = opts.statefile
+  self.psyPath = opts.psyPath
+
+  self.group = respawn()
+  self.extra = {}
+  self.logging = {}
+  self.linestate = {}
+  self.group.on('stdout', self._ondata.bind(self))
+  self.group.on('stderr', self._ondata.bind(self))
+
+  self.group.on('start', self._onev('start'))
+  self.group.on('stop', self._onev('stop'))
+  self.group.on('restart', self._onev('restart'))
+  self.group.on('crash', self._onev('crash'))
+  self.group.on('sleep', self._onev('sleep'))
+  self.group.on('spawn', self._onev('spawn', 'PID %d', 'pid'))
+  self.group.on('exit', self._onev('exit', '%s'))
+  self.group.on('warn', self._onev('warn', '%s'))
+
+  self._readState(function (err, state) {
+    if (err) return self.emit('error', err)
+    state.forEach(function (e) {
+      self.start(e.id, e.command, {
+        cwd: e.cwd,
+        env: e.env,
+        logfile: e.logfile, 
+        maxRestarts: e.maxRestarts,
+        sleep: e.sleep
+      })
+    })
+  })
+}
+
+Psy.prototype._onev = function (name, fmt) {
+  var self = this
+  name = name.toUpperCase()
+  var props = [].slice.call(arguments, 2)
+
+  return function (mon) {
+    if (!has(self.logging, mon.id)) return
+    var outputs = self.logging[mon.id]
+    var args = [].slice.call(arguments, 1)
+    for (var i = 0; i < props.length; i++) {
+      args[i] = args[i][props[i]]
+    }
+
+    outputs.forEach(function (out) {
+      var pre = (self.linestate[mon.id] ? '' : '\n') + '!!! PROCESS '
+      if (fmt) {
+        var str = sprintf.apply(null, [fmt].concat(args).filter(Boolean))
+        out.write(pre + name + ': ' + str + '\n')
+      } else {
+        out.write(pre + name + '\n')
+      }
+      self.linestate[mon.id] = true
+    })
+  }
+}
+
+Psy.prototype._checkpoint = function (cb) {
+  var self = this
+  if (typeof cb !== 'function') cb = noop
+  var src = JSON.stringify(self.group.list().map(function (e) {
+    return {
+      id: e.id,
+      status: e.status,
+      command: e.command,
+      cwd: e.cwd,
+      env: e.env,
+      maxRestarts: e.maxRestarts,
+      sleep: e.sleep,
+      extra: self.extra[e.id]
+    }
+  }))
+  fs.writeFile(self.statefile, src, cb)
+}
+
+Psy.prototype._ondata = function (mon, buf) {
+  if (!has(this.logging, mon.id)) return
+  var outputs = this.logging[mon.id]
+  outputs.forEach(function (out) { out.write(buf) })
+  this.linestate[mon.id] = buf[buf.length-1] === 10 // \n
+}
+
+Psy.prototype.list = function (cb) {
+  var self = this
+  var items = self.group.list().map(function (item) {
+    var ref = xtend(item, {})
+    delete ref.domain
+    delete ref.child
+    delete ref._events
+    delete ref._eventsCount
+    delete ref._maxListeners
+    delete ref.timeout
+
+    if (has(self.extra, item.id)) return xtend(ref, self.extra[item.id])
+    else return ref
+  })
+  if (typeof cb === 'function') cb(null, items)
+}
+
+Psy.prototype.start = function (name, command, opts, cb) {
+  var self = this
+  if (typeof opts === 'function') {
+    cb = opts
+    opts = {}
+  }
+  if (typeof cb !== 'function') cb = noop
+  cb = once(cb)
+
+  var gs = self.group.list()
+  for (var i = 0; i < gs.length; i++) {
+    if (gs[i].id !== name) continue
+    if (gs[i].status === 'running') {
+      return cb(errinfo('A process called ' + JSON.stringify(name)
+        + ' is already running.'))
+    } else {
+      self.group.remove(name, function (err) {
+        if (err) cb(err)
+        else cstart(done)
+      })
+    }
+    return
+  }
+  cstart(done)
+
+  function done (err) {
+    if (err) return cb(err)
+    self._checkpoint(cb)
+  }
+
+  function cstart (cb) {
+    self.extra[name] = {}
+    if (opts.logfile && !has(self.logging, name)) {
+      var w = fs.createWriteStream(opts.logfile, { flags: 'a' })
+      w.once('error', cb)
+      self.logging[name] = [ w ]
+      self.linestate[name] = true
+      self.extra[name].logfile = opts.logfile
+    }
+    if (!self.group.get(name)) self.group.add(name, command, opts)
+    self.group.start(name, opts)
+    cb()
+  }
+}
+
+Psy.prototype.stop = function (name, cb) {
+  var self = this
+  self.group.stop(name, function (err) {
+    if (err) return cb(err)
+    self._checkpoint(cb)
+  })
+}
+
+Psy.prototype.restart = function (name, cb) {
+  this.group.restart(name)
+  this._checkpoint(cb)
+}
+
+Psy.prototype.remove = function (name, cb) {
+  this.group.remove(name)
+  this._checkpoint(cb)
+}
+
+Psy.prototype.log = function (name, opts) {
+console.log('LOG', name, self.extra)
+  var self = this
+  if (!opts) opts = {}
+  if (!has(self.logging, name)) {
+    self.logging[name] = []
+    self.linestate[name] = true
+  }
+  var stream = through()
+  if (defined(opts.n, opts.N) !== undefined) {
+    if (self.extra[name] && self.extra[name].logfile) showlines()
+    else if (!opts.follow) stream.end()
+  }
+  if (defined(opts.n, opts.N) === undefined || opts.follow) {
+    self.logging[name].push(stream)
+    onend(stream, function () {
+      var ix = self.logging[name].indexOf(stream)
+      if (ix >= 0) self.logging[name].splice(ix, 1)
+    })
+  }
+  return stream
+
+  function showlines () {
+    stream.pause()
+    var sf = sliceFile(self.extra[name].logfile)
+    var args = []
+    if (/,/.test(opts.n)) {
+      args = opts.n.split(',').map(function (s) {
+        return -Number(s)
+      })
+    } else if (opts.n !== undefined) {
+      args[0] = -opts.n
+    } else if (/,/.test(opts.N)) {
+      args = opts.N.split(',').map(Number)
+    } else {
+      args[0] = opts.N
+    }
+    sf.slice.apply(sf, args).pipe(through(writec, function () {
+      stream.resume()
+      if (self.logging[name].indexOf(stream) < 0) {
+        stream.end()
+      }
+    }))
+  }
+}
+
+Psy.prototype._readState = function (cb) {
+  fs.readFile(this.statefile, 'utf8', function (err, src) {
+    if (err && err.code === 'ENOENT') src = '[]'
+    else if (err) return cb(err)
+
+    try { var state = JSON.parse(src || '[]') }
+    catch (err) { return cb(err) }
+
+    if (!Array.isArray(state)) cb(null, [])
+    else cb(null, state)
+  })
+}
+
+function errinfo (msg) {
+  var err = new Error(msg)
+  err.info = msg
+  return err
+}
+function noop () {}
