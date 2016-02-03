cd55d3a2a2c3e02a15b096d7ff0b241c654053d9
Max Ogden <max@maxogden.com>
2016-02-03 15:41:55 -0800

    add failing test for pid stability

--- test/run.js
+++ test/run.js
@@ -40,9 +40,49 @@ test('run', function (t) {
   }
 })
 
-test('run teardown', function (t) {
+test('rm time', function (t) {
   t.plan(1)
   run('psy rm time', function (err) {
+    t.ifError(err)
+  })
+})
+
+test('long lived process pid is stable', function (t) {
+  run('psy start -n time2'
+    + ' -l ' + path.join(env.PSY_PATH, 'time2.log')
+    + ' -- bash -c "while true; do date; sleep 0.1; done"',
+    ready)
+  function ready (err) {
+    t.ifError(err)
+    getPid(function (err, pidA) {
+      t.ifError(err)
+      t.ok(pidA, 'get first pid')
+      setTimeout(function () {
+        getPid(function (err, pidB) {
+          t.ifError(err)
+          t.ok(pidB, 'get second pid')
+          t.equal(pidA, pidB, 'pids match')
+          t.end()
+        })
+      }, 1000)
+    })
+  }
+
+  function getPid (cb) {
+    run('psy ls', function (err, stdout, stderr) {
+      if (err) return cb(err)
+      var rows = stdout.split('\n').filter(Boolean).map(function (line) {
+        return line.split('  ')
+      })
+      if (!rows.length) return cb(new Error('psy ls returned no results'))
+      cb(null, rows[0][2])
+    })
+  }
+})
+
+test('rm time2', function (t) {
+  t.plan(1)
+  run('psy rm time2', function (err) {
     t.ifError(err)
   })
 })
